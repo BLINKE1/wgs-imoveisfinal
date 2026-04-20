ec2bed90fb78e4377f7dd835145382892b67ddeb
BLINKE1 <will_ftec@hotmail.com>
2026-04-19 22:05:01 -0300

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0152b9ab3bca3c89"; }
+/******/ 		__webpack_require__.h = function() { return "2ee6927f062cfe85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
